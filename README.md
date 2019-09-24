f5ca358cb01d504795be648c35082d386200d734
hajerbin <974088545@qq.com>
2019-09-24 18:17:00 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mdtohhtml
 a package convert markdown file to html file , with some css in one single file
+# how to use
+- `npm install md-pillar -g`,set `mdtohtml` as global command
+- example:`mdtohtml path_of_file.md dir_of_target`
+- `path_of_file.md` can relative path or absolute path,if there is no, will default as relative `./index.md`
+- `dir_of_target` can relative path or absolute path ,if there is no, will default as `./`
--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const gulpMarkdownToHTML = require('@wulechuan/gulp-markdown-to-html')
 const path = require('path');
 
 
-    module.exports = ()=>{
-        /* 
-        1.两个参数一个是需要编译的文件，一个是目标文件包含文件夹。
-        2.如果没有指定需要编译的文件，默认当前目录下的index.md,如果不存在，报错。
-        3.如果没有指定目标文件，默认当前目录下的index.html.
-        */
-    //    console.log(path.resolve(__dirname,'/love/index.js'));
+module.exports = ()=>{
+    /* 
+    compileFile : default as ./index.md
+    targetDir : default as current dir.
+    */
 
-       var args = process.argv.slice(2);
-       var compileFile =args[0] || './index.md';
-       var targetDir = args[1] || './';
-       gulp.src(compileFile).pipe(gulpMarkdownToHTML()).pipe(gulp.dest(targetDir));
+    var args = process.argv.slice(2);
+    var compileFile =args[0] || './index.md';
+    var targetDir = args[1] || './';
+    gulp.src(compileFile).pipe(gulpMarkdownToHTML()).pipe(gulp.dest(targetDir));
 
-       console.log(`compile succeed at ${path.resolve(targetDir)}`);
-    }
+    console.log(`compile succeed at ${path.resolve(targetDir)}`);
+}
